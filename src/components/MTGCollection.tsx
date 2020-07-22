70b0f64fa3d52a93ca2d99163ca3f0a35b0eef02
Patrick <pkhwong.92@gmail.com>
2020-07-21 20:44:06 -0700

    Added sum feature

--- src/components/MTGCollection.tsx
+++ src/components/MTGCollection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/MTGCollection.scss'
 
 type Rarity = 'M' | 'R' | 'U' | 'C'
 type Style = 'Regular' | 'Extended' | 'Showcase' | 'Alternate'
@@ -26,7 +27,7 @@ const data: Card[] = [
     price: 109.99
   },
   {
-    name: 'Teferi',
+    name: 'Teferi, Master of Time',
     image: 'https://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid=489167',
     num: 277,
     expansion: 'M21',
@@ -34,25 +35,52 @@ const data: Card[] = [
     style: 'Showcase' as const,
     foil: true,
     price: 69.99
+  },
+  {
+    name: 'Baneslayer Angel',
+    image: 'https://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid=485329',
+    num: 6,
+    expansion: 'M21',
+    rarity: 'M' as const,
+    style: 'Regular' as const,
+    foil: true,
+    price: 5.99
   }
 ]
 
 const isFoil = (condition: boolean): string => {
-  if (condition)
-    return "Foil"
-  return "Regular"
+  return condition? "Foil" : "Regular";
+}
+
+const ResaleValue = (retail_price: number): number => {
+  let percentage: number = 0.80;
+  return percentage*retail_price;
+}
+
+const TotalValue = (collection: Card[]): number => {
+  let sum: number = 0;
+  collection.map(e => sum += ResaleValue(e.price))
+  return sum;
 }
 
 export const MTGCollection: React.FC<{}> = () => {
   return (
-    <div>
-      <h1>My collection</h1>
+    <div className='collection'>
+      <h1>My Collection</h1>
+      <h2>Total Collection Value: {TotalValue(data).toFixed(2)}</h2>
       {data.map(e => {
         return (
           <table>
               <tr>
                 <td><img alt={e.name} src={e.image} /></td>
-                <td><div>{e.name}, {e.num}, {e.expansion}, {e.rarity}, {isFoil(e.foil)} {e.price}</div></td>
+                <td>
+                  <ul className='card-info'>
+                    <li><h1>{e.name}</h1></li>
+                    <li>{e.expansion}, {e.rarity}, {isFoil(e.foil)}</li>
+                    <li>F2F Price: {e.price}</li>
+                    <li>80% F2F: ${ResaleValue(e.price).toFixed(2)}</li>
+                  </ul>
+                </td>
               </tr>
           </table>
         )
