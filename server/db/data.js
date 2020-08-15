18c766b4f513103b5997a3b3e310cb5a9ea71183
Patrick <pkhwong.92@gmail.com>
2020-08-15 13:24:54 -0700

    Added more card entries

--- server/db/data.js
+++ server/db/data.js
@@ -1,22 +1,22 @@
 const data = [
-  // {
-  //   name: 'Ugin, the Spirit Dragon',
-  //   expansion: 'M21',
-  //   style: 'Showcase',
-  //   foil: true
-  // },
-  // {
-  //   name: 'Teferi, Master of Time',
-  //   expansion: 'M21',
-  //   style: 'Showcase',
-  //   foil: true
-  // },
-  // {
-  //   name: 'Baneslayer Angel',
-  //   expansion: 'M21',
-  //   style: 'Regular',
-  //   foil: true
-  // },
+  {
+    name: 'Ugin, the Spirit Dragon',
+    expansion: 'M21',
+    style: 'Showcase',
+    foil: true
+  },
+  {
+    name: 'Teferi, Master of Time',
+    expansion: 'M21',
+    style: 'Showcase',
+    foil: true
+  },
+  {
+    name: 'Baneslayer Angel',
+    expansion: 'M21',
+    style: 'Regular',
+    foil: true
+  },
   {
     name: 'Grim Tutor',
     expansion: 'M21',
@@ -28,31 +28,31 @@ const data = [
     expansion: 'RNA',
     style: 'Regular',
     foil: false
+  },
+  {
+    name: 'Terror of the Peaks',
+    expansion: 'M21',
+    style: 'Regular',
+    foil: false
+  },
+  {
+    name: 'Crucible of Worlds',
+    expansion: 'M19',
+    style: 'Regular',
+    foil: false
+  },
+  {
+    name: 'Parallel Lives',
+    expansion: 'ISD',
+    style: 'Regular',
+    foil: false
+  },
+  {
+    name: 'Chandra, Heart of Fire',
+    expansion: 'M21',
+    style: 'Borderless',
+    foil: true
   }
-  // {
-  //   name: 'Terror of the Peaks',
-  //   expansion: 'M21',
-  //   style: 'Regular',
-  //   foil: false
-  // },
-  // {
-  //   name: 'Crucible of Worlds',
-  //   expansion: 'M19',
-  //   style: 'Regular',
-  //   foil: false
-  // },
-  // {
-  //   name: 'Parallel Lives',
-  //   expansion: 'ISD',
-  //   style: 'Regular',
-  //   foil: false
-  // },
-  // {
-  //   name: 'Chandra, Heart of Fire',
-  //   expansion: 'M21',
-  //   style: 'Borderless',
-  //   foil: true
-  // }
 ]
 
 module.exports = data;
