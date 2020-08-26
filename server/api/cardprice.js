8dd437e34c00b9f9792d2c8453fce224e37af57b
Patrick <pkhwong.92@gmail.com>
2020-08-26 00:18:42 -0700

    Attempt to improve fetch efficiency

--- server/api/cardprice.js
+++ server/api/cardprice.js
@@ -4,7 +4,7 @@ const querybuilder = require('../helper/querybuilder');
 const resalevalue = require('../helper/resalevalue');
 const puppeteer = require('puppeteer');
 
-router.get('/all', async(req, res) => {
+router.get('/all', async (req, res) => {
 
   const processData = async (card_name, expansion, foil, style) => {
     
@@ -17,26 +17,45 @@ router.get('/all', async(req, res) => {
 
     const query = querybuilder(card_name, expansion, style);
     let URL = `https://www.facetofacegames.com/${query}/`;
+    let browser = null;
 
-    const browser = await puppeteer.launch({headless: true});
-    const page = await browser.newPage();
-    await page.goto(URL, {waitUntil: 'networkidle2'});
+    try {
+
+      browser = await puppeteer.launch({headless: true});
+      const page = await browser.newPage();
+
+      //turns request interceptor on
+      await page.setRequestInterception(true);
+
+      //if the page makes a request to a resource type of image or stylesheet then abort that            request
+      page.on('request', request => {
+          if (request.resourceType() === 'image' || request.resourceType() === 'stylesheet')
+              request.abort();
+          else
+              request.continue();
+      });
+
+      await page.goto(URL, {waitUntil: 'networkidle2'});
+    
+      await Promise.all([
+        foil? page.evaluate(() => document.querySelector("input[data-label='Foil']").click()):
+        page.evaluate(() => document.querySelector("input[data-label='Non-Foil']").click()),
+        page.waitFor(1000)
+      ])
+    
+      let data = await page.evaluate(() => {
+        let price = document.querySelector('.price--withoutTax').innerText
+        return price;  
+      });
   
-    await Promise.all([
-      foil? page.evaluate(() => document.querySelector("input[data-label='Foil']").click()):
-      page.evaluate(() => document.querySelector("input[data-label='Non-Foil']").click()),
-      page.waitFor(1000)
-    ])
-  
-    let data = await page.evaluate(() => {
-      let price = document.querySelector('.price--withoutTax').innerText
-      return price;  
-    });
+      obj['price'] = await data.substring(1);
+      obj['estvalue'] = resalevalue(parseFloat(data.substring(1)));
 
-    obj['price'] = await data.substring(1);
-    obj['estvalue'] = resalevalue(parseFloat(data.substring(1)));
-
-    await page.close();
+    } catch (e) {
+      console.error(e)
+    } finally {
+      if (browser) browser.close();
+    }
 
     return obj;
   }
