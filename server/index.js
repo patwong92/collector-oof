c76c0a0d94eaff5931ad8746140918acaad02931
Patrick <pkhwong.92@gmail.com>
2020-09-17 14:58:52 -0700

    Removed package-lock.json

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const path = require('path')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
